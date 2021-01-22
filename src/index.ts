c1e1825dceabb42742454cfd3a6d905469743306
Mechislav <id110099122@gmail.com>
2021-01-23 00:25:15 +0200

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import debug from 'debug';
 import hexFormat from './formats/hex';
 
 debug.formatters.h = hexFormat;
-debug.formatters.H = v => '<' + hexFormat(v).toUpperCase() + '>';
+debug.formatters.H = (v) => '<' + hexFormat(v).toUpperCase() + '>';
 
 export const createLogger = (namespace: string = 'default') => debug(namespace);
 export const log = createLogger();
